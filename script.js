a97c3f149cff0a00d4dff59c3ab610c08c258d48
Cheremsha44 <r.chulkov44@gmail.com>
2026-05-25 18:57:39 +0500

    feat(js): add function calculate witchout choice operator

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // Make eventlistener to console.log(button.value)
 // Make eventlistener to console.log(input.value)
 
+
+// 1) Vvod in input
+// 2) Click button operator {firstValue} and {operator}
+// 3) Vvod in input {secondValue}
+// 4) Click button calculate {secondValue}
+// 5) Calculate {firstValue} and {operator} and {secondValue}
+//---------------------------------------------------------------------------------------------------------
 //HTML
 const inputValue = document.querySelector('#inputValue')
 const operatorButtons = document.querySelectorAll('.operator')
@@ -9,11 +16,18 @@ const calculate = document.querySelector('.ravno')
 let firstValue = 0
 let secondValue = 0
 let operator = ''
+let result = 0
 // eventListener
 operatorButtons.forEach((button) => {
     button.addEventListener("click", (e) => {
-        console.log(button.id)
-        firstValue = inputValue.value
-        console.log(firstValue)
+        operator = button.id
+        firstValue = parseInt(inputValue.value, 10)
+        console.log(`${firstValue} ${operator}`)
     })
+})
+
+calculate.addEventListener("click", (e) => {
+    secondValue = parseInt(inputValue.value, 10)
+    result = firstValue + secondValue
+    console.log(result)
 })
